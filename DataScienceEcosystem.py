02a120fce524b4304e3dbcba1de7c421165b8949
UmitKaynar <umitkaynar35@gmail.com>
2023-07-08 13:32:49 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+
+# unordered list:List popular languages for Data Science, List commonly used libraries used by Data Scientists, List common Data Science tools 
+
+# Some of the popular languages that Data Scientists use are
+
+# ordered list: 1-Pyhton, 2-R, 3-Scala, 4-Java
+
+# Some of the commonly used libraries used by Data Scientists include: 
+
+# ordered list: Pandas, Numpy, Matplotlib, TensorFlow, Seaborn, Scikit-learn, Pytorch, Keras
+
+# | Data Science Tools |
+# |--------------------|
+# | Development Environment 1- Jupiter Notebook  |
+# | Development Environment 2- Anaconda          |
+# | Development Environment 3- RStudio           |
+# | Development Environment 4- Apache Zeppelin   |
+# | Development Environment 5- Spyder            |
+# | Development Environment 6- Visual Studio Code|
+# | Development Environment 7- IBM Watson Studio |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python in Jupyter Notebook
+
+# In[3]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+(200/60)
+
+
+# **Objectives:**
+
+# - Explore common data manipulation and analysis libraries.
+# - Understand basic concepts of machine learning and its applications.
+# - To enter deep learning and AI concepts
+
+# ## Author
+# 
+# Umit Kaynar
+
+# In[ ]:
+
+
+
+
